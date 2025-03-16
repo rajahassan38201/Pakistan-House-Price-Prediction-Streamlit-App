6ea73ff7b30f1eed29357a9392ed5acec56b1bf0
Hafiz Hassan Abdullah <151431823+rajahassan38201@users.noreply.github.com>
2025-03-17 00:40:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(
 
 st.markdown(
     """
-    <a href="localhost:7067" target="_blank">Back To Home Page</a>
+    <a href="https://localhost:7067" target="_blank">Back To Home Page</a>
     """,
     unsafe_allow_html=True,
 )
